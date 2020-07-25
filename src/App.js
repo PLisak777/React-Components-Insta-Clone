650cc8765cc332bdd440a667b3aec0b9d8fa2ab6
Phil Lisak <phil.lisak@gmail.com>
2020-07-24 18:44:02 -0700

    Got the SearchBar showing

--- src/App.js
+++ src/App.js
@@ -9,24 +9,33 @@ import React, { useState } from 'react';
 // Import the Posts (plural!) and SearchBar components, since they are used inside App component
 import Posts from './components/Posts/Posts';
 import Post from './components/Posts/Post';
-import LikeSection, { likePost, numberOfLikes } from './components/Posts/LikeSection';
+import LikeSection from './components/Posts/LikeSection';
+import SearchBar from './components/SearchBar/SearchBar'
 // Import the dummyData
 import dummyData from './dummy-data';
 import './App.css';
 
-const App = () => {
+const App = (props) => {
   // Create a state called `posts` to hold the array of post objects, **initializing to dummyData**.
-  const [posts] = useState(dummyData); // This state is the source of truth for the data inside the app. You won't be needing dummyData anymore.
-  console.log('posts', posts); // testing
+  const [posts] = useState(dummyData); 
+  // This state is the source of truth for the data inside the app. You won't be needing dummyData anymore.
   // To make the search bar work (which is stretch) we'd need another state to hold the search term.
 
   const likePost = (postId) => {
     /*
       This function serves the purpose of increasing the number of likes by one, of the post with a given id.
-
+*/
+    // <LikeSection />;
+/*
       The state of the app lives at the top of the React tree, but it wouldn't be fair for nested components not to be able to change state!
       This function is passed down to nested components through props, allowing them to increase the number of likes of a given post.
+*/
 
+    const setPosts = () => {
+      return posts.map(postId);
+    }
+
+    /*
       Invoke `setPosts` and pass as the new state the invocation of `posts.map`.
       The callback passed into `map` performs the following logic:
         - if the `id` of the post matches `postId`, return a new post object with the desired values (use the spread operator).
@@ -36,7 +45,8 @@ const App = () => {
 
   return (
     <div className='App'>
-      {/* Add SearchBar and Posts here to render them */}
+      {<Posts /> /* Add SearchBar and Posts here to render them */}
+      {<SearchBar />}
       {/* Check the implementation of each component, to see what props they require, if any! */}
     </div>
   );
